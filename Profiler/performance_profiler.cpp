637bfd75b29d1ce51badf3461744aa7393e42b24
Erik-Kelemen <kelemen.erik@gmail.com>
2023-08-02 18:57:23 -0400

    Refactoring class definition to header

--- Profiler/performance_profiler.cpp
+++ Profiler/performance_profiler.cpp
@@ -1,41 +1,32 @@
-#include <iostream>
-#include <chrono>
-#include <string>
-#include <unordered_map>
+// profiler.cpp
 
-class Profiler {
-private:
-    std::unordered_map<std::string, double> componentTimes_;
-    std::chrono::steady_clock::time_point startTime_;
+#include "performance_profiler.h"
 
-public:
-    Profiler() {
-        startTime_ = std::chrono::steady_clock::now();
+Profiler::Profiler() {
+    startTime_ = std::chrono::steady_clock::now();
+}
+
+void Profiler::startComponent(const std::string& componentName) {
+    componentTimes_[componentName] = 0.0;
+    componentTimes_[componentName] -= getCurrentTime();
+}
+
+void Profiler::stopComponent(const std::string& componentName) {
+    componentTimes_[componentName] += getCurrentTime();
+}
+
+double Profiler::getTotalTime() const {
+    return getCurrentTime();
+}
+
+void Profiler::printComponentTimes() const {
+    std::cout << "Component times:" << std::endl;
+    for (const auto& entry : componentTimes_) {
+        std::cout << entry.first << ": " << entry.second << " seconds" << std::endl;
     }
+}
 
-    void startComponent(const std::string& componentName) {
-        componentTimes_[componentName] = 0.0;
-        componentTimes_[componentName] -= getCurrentTime();
-    }
-
-    void stopComponent(const std::string& componentName) {
-        componentTimes_[componentName] += getCurrentTime();
-    }
-
-    double getTotalTime() const {
-        return getCurrentTime();
-    }
-
-    void printComponentTimes() const {
-        std::cout << "Component times:" << std::endl;
-        for (const auto& entry : componentTimes_) {
-            std::cout << entry.first << ": " << entry.second << " seconds" << std::endl;
-        }
-    }
-
-private:
-    double getCurrentTime() const {
-        auto currentTime = std::chrono::steady_clock::now();
-        return std::chrono::duration<double>(currentTime - startTime_).count();
-    }
-};
+double Profiler::getCurrentTime() const {
+    auto currentTime = std::chrono::steady_clock::now();
+    return std::chrono::duration<double>(currentTime - startTime_).count();
+}
---
+++ Profiler/performance_profiler.h
@@ -0,0 +1,26 @@
+#ifndef PROFILER_H
+#define PROFILER_H
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <unordered_map>
+
+class Profiler {
+private:
+    std::unordered_map<std::string, double> componentTimes_;
+    std::chrono::steady_clock::time_point startTime_;
+
+public:
+    Profiler();
+
+    void startComponent(const std::string& componentName);
+    void stopComponent(const std::string& componentName);
+    double getTotalTime() const;
+    void printComponentTimes() const;
+
+private:
+    double getCurrentTime() const;
+};
+
+#endif 
