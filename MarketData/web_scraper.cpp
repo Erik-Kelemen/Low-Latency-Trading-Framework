0d0ac2e80b0689dcaaccef849c2a79b4e7393702
Erik-Kelemen <kelemen.erik@gmail.com>
2023-08-04 21:28:52 -0400

    Doxygen documentation and refactoring file load to util.h

--- MarketData/web_scraper.cpp
+++ MarketData/web_scraper.cpp
@@ -5,6 +5,7 @@
 
 #include "../Profiler/performance_profiler.h"
 #include "../stock_price.h" // Include the StockPrice header
+#include "../util.h"
 #include <vector>
 
 #include <fstream>
@@ -18,46 +19,55 @@ const std::string BASE_URL = "https://www.alphavantage.co/query?";
 const std::string API_KEY = "ALQU3SWWYFF7QHXA"; //replace with your Alpha Vantage API Key
 const std::string interval = "1min";
 
+/**
+ * @brief Callback function used by libcurl to write fetched data into a std::string.
+ *
+ * @param contents Pointer to the data received from the HTTP request.
+ * @param size Size of each data element.
+ * @param nmemb Number of data elements.
+ * @param response Pointer to the std::string to store the fetched data.
+ * @return Total size of the data received.
+ */
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* response) {
     size_t totalSize = size * nmemb;
     response->append(static_cast<char*>(contents), totalSize);
     return totalSize;
 }
 
+/**
+ * @brief Scrapes stock data for a given list of symbols and target date.
+ *
+ * @param symbols Vector of stock symbols to scrape data for.
+ * @param targetDate The target date for which data is to be scraped.
+ * @param profiler Profiler to measure performance.
+ */
 void scrape(std::vector<std::string> symbols, std::string targetDate, Profiler profiler){
     profiler.startComponent("Web Scraper");
-    std::ofstream outputFile("exchange_prices.csv");
-    if (outputFile.is_open()) {
-        std::streambuf* originalBuffer = std::cout.rdbuf(); 
-        std::cout.rdbuf(outputFile.rdbuf()); 
-        
-        std::vector<StockPrice> prices;
-        
-        for (const std::string& symbol : symbols) {
-            std::string jsonData = fetchStockData(symbol);
-            std::vector<StockPrice> cur_prices = parseStockData(symbol, jsonData, targetDate);
-            prices.insert(prices.end(), cur_prices.begin(), cur_prices.end());
-        }
-        std::cout << "ticker,time,price" << std::endl;
-        for(StockPrice p: prices){
-            p.print();
-        }
+    
+    std::vector<StockPrice> prices;
 
-        std::cout.rdbuf(originalBuffer);
-        
-        outputFile.close();
-        std::cout << "Data written to file successfully." << std::endl;
-    } else {
-        std::cerr << "Error opening the file!" << std::endl;
+    for (const std::string& symbol : symbols) {
+        std::string jsonData = fetchStockData(symbol);
+        std::vector<StockPrice> cur_prices = parseStockData(symbol, jsonData, targetDate);
+        prices.insert(prices.end(), cur_prices.begin(), cur_prices.end());
     }
+    
+    write("ticker,date,price", "exchange_prices.csv", prices);
     
     profiler.stopComponent("Web Scraper");
 }
 
+/**
+ * @brief Fetches stock data for a given symbol from the Alpha Vantage API.
+ *
+ * @param symbol The stock symbol to fetch data for.
+ * @return The fetched stock data as a JSON string.
+ */
 std::string fetchStockData(const std::string& symbol) {
     std::string url = BASE_URL + "function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&apikey=" + API_KEY + "&interval=" + interval + "&extended_hours=false&outputsize=full";
     CURL* curl = curl_easy_init();
     std::string response;
+
     if (curl) {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
@@ -75,6 +85,15 @@ std::string fetchStockData(const std::string& symbol) {
     
     return response;
 }
+
+/**
+ * @brief Parses the fetched stock data and extracts relevant information.
+ *
+ * @param ticker The stock symbol.
+ * @param jsonData The JSON data received from the API.
+ * @param targetDate The target date to filter data for.
+ * @return A vector of StockPrice objects containing the parsed data.
+ */
 std::vector<StockPrice> parseStockData(const std::string ticker, const std::string& jsonData, const std::string& targetDate) {
     std::vector<StockPrice> stockData;
 
@@ -99,7 +118,7 @@ std::vector<StockPrice> parseStockData(const std::string ticker, const std::stri
     return stockData;
 }
 
-//g++ -std=c++17 -Wall -Wextra -I/home/mars/rapidjson/include -I../Profiler -I../ web_scraper.cpp ../stock_price.cpp ../Profiler/performance_profiler.cpp -o web_scraper -lcurl
+//g++ -std=c++17 -Wall -Wextra -I/usr/local/include -I./home/mars/Low-Latency-Trading-Framework web_scraper.cpp ../stock_price.cpp ../util.cpp ../Profiler/performance_profiler.cpp -o web_scraper -lcurl
 int main() {
     std::vector<std::string> symbols = {"MSFT", "AMZN", "GOOGL", "META", "NFLX"}; 
     std::string targetDate = "2023-08-02";
