04251660db2db9776682bc49fcd5c4ce82b0355e
Erik-Kelemen <kelemen.erik@gmail.com>
2023-08-07 22:21:24 -0400

    Moving Profiler declaration to main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include "TradingEngine/controller.cpp"
-
+#include "Profiler/performance_profiler.h"
+#include <vector>
+#include <string>
 int main() {
-    Controller controller;
+    std::vector<std::string> symbols = {"MSFT", "AMZN", "GOOGL", "META", "NFLX"};
+    const std::string date = "2023-08-02";
+    Profiler profiler;
+    Controller controller(symbols, date, profiler);
     controller.runTradingFramework();
-
+    profiler.printComponentTimes();
     return 0;
 }
