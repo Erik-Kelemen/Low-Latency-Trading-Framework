8e3d094f6108c4704e807aee5ced8c9339a3f66d
Erik-Kelemen <kelemen.erik@gmail.com>
2023-08-15 22:40:52 -0400

    Dependency injection of trading engine to controller

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include "TradingEngine/controller.cpp"
+#include "TradingEngine/trading_engine.h"
 #include "Profiler/performance_profiler.h"
 #include <vector>
 #include <string>
 int main() {
+    
     std::vector<std::string> symbols = {"MSFT", "AMZN", "GOOGL", "META", "NFLX"};
     std::vector<std::string> dates = {"2023-08-02"};
     double cash = 1000000.0;
     int lookbackPeriod = 30000;
     Profiler profiler;
-    Controller controller(cash, lookbackPeriod, symbols, dates, profiler);
+    TradingEngine tradingEngine(profiler);
+    Controller controller(tradingEngine, cash, lookbackPeriod, symbols, dates, profiler);
     controller.runTradingFramework();
     profiler.printComponentTimes();
     return 0;
